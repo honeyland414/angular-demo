7d931418ba66254c83f2873b44a684c19c3913e0
honeyland414 <wangkai.honeyland@gmail.com>
2026-02-04 17:29:11 -0500

    add router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { JavaComponent } from './home/courses/java/java.component';
+import { CoursesComponent } from './home/courses/courses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:HomeComponent},
+    {path: 'courses', component:CoursesComponent},
+    {path: 'courses/java', component:JavaComponent},
+    {path: '**', component:NotFoundComponent}
+];
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -7,16 +7,18 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { ThemeService } from '../../services/theme.service';
 import { NzAvatarModule } from 'ng-zorro-antd/avatar';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { RouterLink, RouterLinkActive } from "@angular/router";
 
 interface menuInfo {
   name: string;
+  path: string;
   list?: menuInfo[];
 }
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NzIconModule, NzMenuModule, NzLayoutModule, NzDropDownModule, CommonModule, NzAvatarModule, NzInputModule],
+  imports: [NzIconModule, NzMenuModule, NzLayoutModule, NzDropDownModule, CommonModule, NzAvatarModule, NzInputModule, RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
   styleUrl: './header.component.less'
 })
@@ -25,13 +27,13 @@ export class HeaderComponent {
   constructor(private themeService: ThemeService) {}
 
   menuList:menuInfo[] = [
-    { name: 'HomePage'},
-    { name: 'All Courses', list:[{ name: 'Java'},{ name: 'Python'},{ name: 'TypeScript'},]},
-    { name: 'Unity', list:[{ name: 'Unity 全部课程'},{ name: 'Unity 高薪就业班'},{ name: 'Unity 学习线路'},]},
-    { name: 'Cocos'},
-    { name: '小程序'},
-    { name: 'AI'},
-    { name: '其他'}, 
+    { name: 'HomePage', path: ''},
+    { name: 'All Courses', path: '/courses', list:[{ name: 'Java', path: '/courses/java'}, { name: 'Python', path: '/courses/python'}, { name: 'TypeScript', path: '/courses/typescript'},]},
+    { name: 'Unity', path: '/unity', list:[{ name: 'Unity 全部课程', path: '/unity/all-course'},{ name: 'Unity 高薪就业班', path: '/unity/high-salary-class'},{ name: 'Unity 学习线路', path: '/unity/study-path'},]},
+    { name: 'Cocos', path: '/cocos',},
+    { name: '小程序', path: '/miniprogram'},
+    { name: 'AI', path: '/ai'},
+    { name: '其他', path: '/other'}
   ];
 
   changeTheme(theme:string) {
